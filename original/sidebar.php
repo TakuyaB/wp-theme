3e8f895f35442d0fef5a9fadfa351e641d6f0790
Takuya.B <TakuyaB@TakuyaB.local>
2017-03-19 01:44:46 +0900

    サイドバー：ウィジェットのみに変更

--- original/sidebar.php
+++ original/sidebar.php
@@ -3,19 +3,3 @@
         <?php dynamic_sidebar(); ?>
     </ul>
 </div><!-- sidebarここまで -->
-
-
-<?php if(have_posts()): while(have_posts()): the_post(); ?>
-<div <?php post_class( 'side-posts' ); ?>>
-    <div class="side-post-thumbnail">
-        <p><?php the_post_thumbnail( array(75,75) ); ?></p>
-    </div>
-    <div class="side-post-content">
-        <h1 class="side-post-title">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
-        </h1>
-    </div>
-</div>
-<?php endif; ?>

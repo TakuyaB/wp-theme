aac1b0a26874950b3c1d2fc179145adce6e2b333
Takuya.B <takuya.b@TakuyaB.local>
2017-10-28 00:28:40 +0900

    リニューアルっぽいことをした

--- original/footer.php
+++ original/footer.php
@@ -2,6 +2,5 @@
     <p>Copyright &copy; <?php bloginfo( 'name' ); ?> All right Reserved.</p>
 </div>
 <?php wp_footer(); ?>
-<a href="#top" class="page_top">▲</a>
 </body>
 </html>
--- original/related-entries.php
+++ original/related-entries.php
@@ -17,22 +17,12 @@
 <?php if( $query->have_posts() ): ?>
     <?php while ($query->have_posts()) : $query->the_post(); ?>
     <div class="related-entry">
-        <div class="related-entry-thumb">
-            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
-                <?php if( has_post_thumbnail() ): //サムネイルを持っているとき ?>
-                    <?php echo get_the_post_thumbnail($post->ID, 'thumb200'); //サムネイルを呼び出す ?>
-                <?php else: //サムネイルを持っていないとき ?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/no-img.png" alt="NO IMAGE" title="NO IMAGE" width="200px" />
-                <?php endif; ?>
-            </a>
-        </div><!-- related-entry-thumbここまで -->
-        <div class="related-entry-content">
             <h4 class="related-entry-title">
+                <span><i class="fa fa-hand-o-right" aria-hidden="true"></i></span>
                 <a href="<?php the_permalink(); ?>">
                     <?php the_title(); //記事のタイトル ?>
                 </a>
             </h4>
-        </div><!-- related-entry-contentここまで -->
     </div><!-- related-entryここまで -->
     <?php endwhile; ?>
 <?php else: ?>
